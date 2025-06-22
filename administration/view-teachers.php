0b6846c617a1c24804a6d51e03ae72b55d7e4bb7
mujtaba Baloch <mujtabakalmati@gmail.com>
2025-06-22 17:31:46 +0400

    all pages has been created

---
+++ administration/view-teachers.php
@@ -0,0 +1,12 @@
+<?php include_once '../includes/header.php';?>
+
+
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
+
+                    <!-- Page Heading -->
+                    <h1 class="h3 mb-4 text-gray-800">view Teachers</h1>
+
+                </div>
+                <!-- /.container-fluid -->
+<?php include_once '../includes/footer.php';?>
